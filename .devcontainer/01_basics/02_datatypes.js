43266e57097726c2ca8469c50c8a99a1b941cec9
Amishee Mathur <abmca24078_amishee@banasthali.in>
2025-05-06 12:31:02 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";//treat all Js code as newer version 
+
+// alert(3+3) // we are using nodejs,not browser
+
+console.log(3 + 3); 
+ //code readability should be high 
+
+
+ console.log("Amishee")
+
+
+ let name="amishee"
+ let age=22
+ let isloogedIn = false
+ let state= null
+
+ // number => to power 53
+ // bigint
+ // string =>""
+ // boolean =>true/false
+ // null=>standalone value
+ // undefined => 
+ //symbol =>unique
+
+
+ // object
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null);// object
